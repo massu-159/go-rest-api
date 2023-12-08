a4878a3d290dd4e8192524fca634147e62c9ba2b
massu-159 <magjtwjp@gmail.com>
2023-12-08 22:41:09 +0900

    [update] refs #9 upgrade jwt v5

---
+++ main.go
@@ -0,0 +1,20 @@
+package main
+
+import (
+	"go-rest-api/controller"
+	"go-rest-api/db"
+	"go-rest-api/repository"
+	"go-rest-api/router"
+	"go-rest-api/usecase"
+)
+
+func main() {
+	// DB接続
+	db := db.NewDB()
+	userRepository := repository.NewUserRepository(db)
+	userUseCase := usecase.NewUserUseCase(userRepository)
+	userController := controller.NewUserController(userUseCase)
+	e := router.NewRouter(userController)
+	// サーバー起動
+	e.Logger.Fatal(e.Start(":8080"))
+}
