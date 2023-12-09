c8f6d78966e234836b159d0a9f282af23847d244
massu-159 <magjtwjp@gmail.com>
2023-12-09 11:04:44 +0900

    [update] refs #14 add router for task

--- router/router.go
+++ router/router.go
@@ -2,15 +2,28 @@ package router
 
 import (
 	"go-rest-api/controller"
+	"os"
 
+	echojwt "github.com/labstack/echo-jwt/v4"
 	"github.com/labstack/echo/v4"
 )
 
 // ルーティングを設定する
-func NewRouter(uc controller.IUserController) *echo.Echo {
-	e:=echo.New()
+func NewRouter(uc controller.IUserController, tc controller.ITaskController) *echo.Echo {
+	e := echo.New()
 	e.POST("/signup", uc.SignUp)
 	e.POST("/login", uc.Login)
 	e.POST("/logout", uc.LogOut)
+	t := e.Group("/tasks")
+	// JWTを使用するミドルウェアを設定する
+	t.Use(echojwt.WithConfig(echojwt.Config{
+		SigningKey:  []byte(os.Getenv("SECRET")),
+		TokenLookup: "cookie:token",
+	}))
+	t.GET("", tc.GetAllTasks)
+	t.GET("/:taskId", tc.GetTaskByID)
+	t.POST("", tc.CreateTask)
+	t.PUT("/:taskId", tc.UpdateTask)
+	t.DELETE("/:taskId", tc.DeleteTask)
 	return e
 }
