d53316646d2222f56ad527e297164a1a3ad72d16
massu-159 <magjtwjp@gmail.com>
2023-12-09 11:39:43 +0900

    [update] refs #16 update tasks at main

--- main.go
+++ main.go
@@ -11,10 +11,16 @@ import (
 func main() {
 	// DB接続
 	db := db.NewDB()
+	// ユーザ
 	userRepository := repository.NewUserRepository(db)
 	userUseCase := usecase.NewUserUseCase(userRepository)
 	userController := controller.NewUserController(userUseCase)
-	e := router.NewRouter(userController)
+	// タスク
+	taskRepository := repository.NewTaskRepository(db)
+	taskUseCase := usecase.NewTaskUseCase(taskRepository)
+	taskController := controller.NewTaskController(taskUseCase)
+	// ルーティング
+	e := router.NewRouter(userController, taskController)
 	// サーバー起動
 	e.Logger.Fatal(e.Start(":8080"))
 }
